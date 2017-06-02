22e8f897f611b30a6d12b8e4408e8d6aab553ac5
mrLordi <d.vodotiiets@yandex.ua>
2017-06-02 05:55:47 +0300

    checkMove fixed

--- back_conqueror/src/main/java/com/conqueror/app/service/GameService.java
+++ back_conqueror/src/main/java/com/conqueror/app/service/GameService.java
@@ -104,7 +104,6 @@ public class GameService {
     public QuestionWrapper attackTerritory(long gameId, String userName, long terittoryNumber) {
         Game game = findGameById(gameId);
         game.isBattle = true;
-        notifyAll();
         Question question = questionService.findRandomQuestion();
         game.currentQuestion = question;
         game.attackuser = findUserByUsernameAndGame(userName, game);
@@ -148,15 +147,9 @@ public class GameService {
 //                    e.printStackTrace();
 //                }
 //            }
-            while(!game.isBattle) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
+            while(!game.isBattle);
             while(game.isBattle);
+
             return questionWrapperService.getEmptyWrapper();
         }
 
